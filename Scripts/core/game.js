3424df41c9b01803e66bb31761afbdda1dad0725
Jiabin <jiabintang@hotmail.com>
2019-01-11 18:41:14 -0500

    [Added] CreateJS Support

---
+++ Scripts/core/game.js
@@ -0,0 +1,13 @@
+// IIFE - Immediately Invoked Function Expression 
+(function(){
+
+    function Start() {
+
+    }
+
+    function Update() {
+
+    }
+
+    window.addEventListener("load", Start)
+}())
---
+++ Scripts/core/player.ts
@@ -0,0 +1,12 @@
+enum Direction { Up, Down, Left, Right };
+class Player {
+
+    
+    constructor(private name: string){
+        
+    }
+
+    public Move() {
+
+    }
+}
